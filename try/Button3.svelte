0bec585b9a2a50dbb21006508d3d9a07e8faa632
LizzieGri <65723977+LizzieGri@users.noreply.github.com>
2021-02-16 13:57:11 +0300

    Create Button3.svelte

---
+++ try/Button3.svelte
@@ -0,0 +1,10 @@
+<style>
+	button {
+    height: 4rem;
+    width: 8rem;
+    border-color: #FFFF00;
+    color: #006400;
+    font-size: 1.05rem;
+  }
+</style>
+<button on:click>Нажми на меня</button>
